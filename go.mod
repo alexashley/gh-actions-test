4a40b135c262cdfa663f632361b2262b9f311ae5
Alex Ashley <alexashley15@gmail.com>
2024-04-15 15:46:12 -0400

    feat: create model database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alexashley/gh-actions-test
 
 go 1.22.0
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func generateDiff(db string) string {
+	return `
+DROP TABLE foo;
+`
+}
 
 func main() {
+	file, err := os.Open("./schema.sql")
+	if err != nil {
+		log.Fatalln("error opening file", err)
+	}
+
+	schema, err := io.ReadAll(file)
+	password := os.Getenv("POSTGRES_PASSWORD")
+
+	db, err := sql.Open("postgres", fmt.Sprintf("password=%s", password))
+	if err != nil {
+		log.Fatalln("error opening db connection", err)
+	}
+
+	_, err = db.Exec(string(schema))
+	if err != nil {
+		log.Fatalln("error creating model database", err)
+	}
+
+	query, err := db.Query("SELECT * FROM pg_catalog.pg_tables;")
+	if err != nil {
+		log.Fatalln("error executing schema query", err)
+	}
+
+	var row string
+	for query.Next() {
+		if err = query.Scan(&row); err != nil {
+			log.Fatalln("error scanning row", err)
+		}
+		fmt.Println("row", row)
+	}
+
+	databases := []string{"abc", "foo", "baz"}
+	for _, db := range databases {
+		fmt.Printf("starting diff for '%s' database", db)
+		//diff := generateDiff(db)
+	}
+
 	fmt.Println("hello, world!")
 }
